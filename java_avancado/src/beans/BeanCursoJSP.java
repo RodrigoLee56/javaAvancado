772a8a5e4df9e85b49ca744f1f43b1c930e6a1ff
RodrigoItachi <sousarodrigo56@outlook.com>
2020-11-23 02:34:51 -0300

    foram add três atributos com seus getters e setters

--- java_avancado/src/beans/BeanCursoJSP.java
+++ java_avancado/src/beans/BeanCursoJSP.java
@@ -1,6 +1,46 @@
 package beans;
 
 public class BeanCursoJSP {
+
+	private String nome;
+	private String ano;
+	private String sexo;
+
+	public BeanCursoJSP() {
+		super();
+	}
+
+	public BeanCursoJSP(String nome, String ano, String sexo) {
+		super();
+		this.nome = nome;
+		this.ano = ano;
+		this.sexo = sexo;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getAno() {
+		return ano;
+	}
+
+	public void setAno(String ano) {
+		this.ano = ano;
+	}
+
+	public String getSexo() {
+		return sexo;
+	}
+
+	public void setSexo(String sexo) {
+		this.sexo = sexo;
+	}
+
 	public int calcula(int numero) {
 		return numero * 100;
 	}
