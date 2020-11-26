990395def5d9833be3b9e1c6c8a5b84e880c23b2
RodrigoItachi <sousarodrigo56@outlook.com>
2020-11-26 05:44:44 -0300

    filter

---
+++ java_avancado/src/filters/Filter.java
@@ -0,0 +1,42 @@
+package filters;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+
+import connection.SingleConnection;
+
+@WebFilter(urlPatterns = { "/*" })
+public class Filter implements javax.servlet.Filter {
+
+	private static Connection connectio;
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		try {
+			chain.doFilter(request, response);
+			connectio.commit();
+		}catch (Exception e) {
+			try {
+				connectio.rollback();
+			} catch (SQLException e1) {
+				e1.printStackTrace();
+			}
+		}
+
+	}
+
+	@Override
+	public void init(FilterConfig config) {
+		SingleConnection.getConnection();
+	}
+
+}
