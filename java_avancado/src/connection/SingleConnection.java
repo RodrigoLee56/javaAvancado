05dc5011bd474f8d40840a3b876d5ab659b8e8d9
RodrigoItachi <sousarodrigo56@outlook.com>
2020-11-26 05:12:07 -0300

    foi criada uma classe para fazer a conexão com o banco de dados.

---
+++ java_avancado/src/connection/SingleConnection.java
@@ -0,0 +1,38 @@
+package connection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class SingleConnection {
+	private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
+	private static final String URL = "jdbc:mysql://localhost:3306/bd_java_avancado?useTimezone=true&serverTimezone=UTC";
+	private static final String USER = "root";
+	private static final String PASSWORD = "toor";
+	private static Connection connection = null;
+	
+	static {
+		conectar();
+	}
+	
+	public SingleConnection() {
+		conectar();		
+	}
+	
+	private static void conectar() {
+		try {
+			
+			if (connection == null) {
+				Class.forName(DRIVER);
+				connection = DriverManager.getConnection(URL, USER, PASSWORD);
+				connection.setAutoCommit(false);
+			}
+			
+		} catch (Exception e) {
+			throw new RuntimeException("Erro ao conectar com o banco de dados!");
+		}
+	}
+	
+	public static Connection getConnection() {
+		return connection;
+	}
+}
