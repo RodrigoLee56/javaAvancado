2a0f358311cda9eda3c1767b34ef93bc36b3a2bc
RodrigoItachi <sousarodrigo56@outlook.com>
2020-12-01 00:00:13 -0300

    foram add a parte do idLogin na servlet

--- java_avancado/src/controller/UsuarioServlet.java
+++ java_avancado/src/controller/UsuarioServlet.java
@@ -15,7 +15,7 @@ import model.Login;
 @WebServlet("/salvarUsuarioServlet")
 public class UsuarioServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-
+	RequestDispatcher view;
 	private UsuarioDAO usuarioDAO = new UsuarioDAO();
 
 	public UsuarioServlet() {
@@ -24,6 +24,25 @@ public class UsuarioServlet extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
+
+		String acao = request.getParameter("acao");
+		String user = request.getParameter("user");
+
+		if (acao.equalsIgnoreCase("delete")) {
+			
+			usuarioDAO.delete(user);
+			view = request.getRequestDispatcher("cadastroUsuario.jsp");
+			request.setAttribute("usuarios", usuarioDAO.listar());
+			view.forward(request, response);
+		
+		} else if (acao.equalsIgnoreCase("editar")) {
+		
+			Login login = usuarioDAO.consultar(user);
+			view = request.getRequestDispatcher("cadastroUsuario.jsp");
+			request.setAttribute("user", login);
+			view.forward(request, response);
+		
+		}
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
@@ -33,17 +52,23 @@ public class UsuarioServlet extends HttpServlet {
 		 * A variável login e senha todas do tipo String recebe o dado passado da view
 		 * atráves do request.getParameter("parametroDaView")
 		 */
+		String id = request.getParameter("idLogin");
 		String login = request.getParameter("login");
-		String senha = request.getParameter("password");
+		String senha = request.getParameter("senha");
 
 		Login usuario = new Login();
+		usuario.setIdLogin(!id.isEmpty() ? Integer.parseInt(id) : 0);
 		usuario.setLogin(login);
 		usuario.setSenha(senha);
 
 		/* usuarioDao.salvar recebe um objeto do tipo Login */
-		usuarioDAO.salvar(usuario);
+		if (id == null || id.isEmpty()) {
+			usuarioDAO.salvar(usuario);
+		} else {
+			usuarioDAO.atualizar(usuario);
+		}
 
-		RequestDispatcher view = request.getRequestDispatcher("cadastroUsuario.jsp");
+		view = request.getRequestDispatcher("cadastroUsuario.jsp");
 		request.setAttribute("usuarios", usuarioDAO.listar());
 		view.forward(request, response);
 	}
